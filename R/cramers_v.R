71436304aabf3c3b0a68e1cc51e2d3a06fb5e7f6
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-05-07 11:32:10 +0100

    fixed typos, changed name of cramers v function

--- R/cramers_v.R
+++ R/cramers_v.R
@@ -1,14 +1,14 @@
 #' Cramer's V
 #'
-#' This function calculates Cramer's V from an abject created by the
+#' This function calculates Cramer's V from an object created by the
 #' \code{\link{chisq.test}} function from the \code{stats} package.
 #' @param x An abject created by \code{\link{chisq.test}}.
 #' @keywords frequency tables, measures of association, chi-square test
 #' @export
 #' @examples
-#' cv_test(x)
+#' cramersv(x)
 
-cv_test = function(x)
+cramersv = function(x)
 {
     CV = sqrt(x$statistic /
                   (sum(x$observed) * (min(ncol(x$observed),
