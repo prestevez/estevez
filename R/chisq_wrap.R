d42a568f6bec681f10211fb36aaa1651980db347
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-05-07 11:39:42 +0100

    fixed an indentation problem in chisq_wrap that prevented the function form being exported

--- R/chisq_wrap.R
+++ R/chisq_wrap.R
@@ -1,24 +1,24 @@
 #' Chi-square wrapper
 #'
 #' This function is wraps the \code{\link{chisq.test}} function
-#'  from the \code{stats} package to automatically simulate the
-#'  p-value if the data provided does not comply with the assumptions
-#'  of the Chi-square test, specifically that there is at least
-#'  one cell with less than 5 expected counts.
-#'  @param x a numeric vector or matrix. x and y can also both be factors.
-#'  @param y a numeric vector; ignored if x is a matrix. If x is a factor,
-#'      y should be a factor of the same length.
-#'  @param ... passes options to the function
-#'  @keywords chi-square
-#'  @export
-#'  @examples
-#'  chisq_wrap(x, y)
+#' from the \code{stats} package to automatically simulate the
+#' p-value if the data provided does not comply with the assumptions
+#' of the Chi-square test, specifically that there is at least
+#' one cell with less than 5 expected counts.
+#' @param x a numeric vector or matrix. x and y can also both be factors.
+#' @param y a numeric vector; ignored if x is a matrix. If x is a factor,
+#'     y should be a factor of the same length.
+#' @param ... passes options to the function
+#' @keywords chi-square
+#' @export
+#' @examples
+#' chisq_wrap(x, y)
 #'
-#'  # x is a matrix or an object that can be coerced into a matrix
-#'  chisq_wrap(x)
+#' # x is a matrix or an object that can be coerced into a matrix
+#' chisq_wrap(x)
 #'
-#'  # Pass number of replicates to be used if simulation is required
-#'  chisq_wrap(x, B = 1000)
+#' # Pass number of replicates to be used if simulation is required
+#' chisq_wrap(x, B = 1000)
 
 chisq_wrap <- function(x, y = NULL, ...)
 {
