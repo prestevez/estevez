6bec37de9b2c9215c5ab6432dc892919c5758234
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-05-09 00:56:31 +0100

    finished printing options (would be best as a method, but that will come later) for victim_table

--- R/victim_table.R
+++ R/victim_table.R
@@ -1,8 +1,8 @@
 #' Vicimisation distribution table
 #'
 
-victim_table <- function(x, data = NULL, print_option = c("none","markdown",
-                         "pandoc", "latex", "html"), repeats = FALSE)
+victim_table <- function(x, data = NULL, repeats = FALSE, print_option =
+                             c("none","markdown", "pandoc", "latex", "html"))
 {
     if(is.data.frame(data)) {xvar <- data[,x]; xname <- x }
     else {xvar <- x; xname <- deparse(substitute(x))}
@@ -22,6 +22,22 @@ victim_table <- function(x, data = NULL, print_option = c("none","markdown",
     distri$"Incident_%" <- prop.table(distri$Incidence) * 100
     distri[1,"Incident_%"] <- NA
 
-    print(xname)
-    return(distri)
+    # Repeats option HERE
+    if(repeats == TRUE) {distype <- "Repeat victimisation "}
+    else {distype <- "Victimisation "}
+
+    if(print_option[1] %in% c("markdown", "pandoc", "latex", "html"))
+    {
+        legend <- "distribution of "
+        legend <- paste(distype, legend, sep = "")
+        cap <- paste(legend, xname, sep = "")
+        cap <- paste(cap, ".", sep = "")
+
+        results <- knitr::kable(distri, format = print_option[1], caption = cap,
+                                digits = 3)
+    } else {results <- distri}
+
+    if(!print_option[1] %in% c("none", "markdown", "pandoc", "latex", "html"))
+    {stop("Printing method does not exist.")}
+    return(results)
 }
