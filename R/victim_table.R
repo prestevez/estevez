3e7ef3e4bbc3c570a7d30b40b50eff2aefc8fca0
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-05-09 01:25:11 +0100

    finished bulk of victim_table function, lapply method works well; still need to write documentation

--- R/victim_table.R
+++ R/victim_table.R
@@ -1,8 +1,8 @@
 #' Vicimisation distribution table
 #'
 
-victim_table <- function(x, data = NULL, repeats = FALSE, print_option =
-                             c("none","markdown", "pandoc", "latex", "html"))
+victim_table <- function(x, data = NULL, print_option = c("none",
+                          "markdown", "pandoc", "latex", "html"))
 {
     if(is.data.frame(data)) {xvar <- data[,x]; xname <- x }
     else {xvar <- x; xname <- deparse(substitute(x))}
@@ -15,21 +15,20 @@ victim_table <- function(x, data = NULL, repeats = FALSE, print_option =
 
     distri$Events <- as.integer(as.character(distri$Events))
     distri$Incidence <- distri$Events * distri$Prevalence
+    distri$Repeats <- distri$Incidence - distri$Prevalence
 
     distri$"Target_%" <- prop.table(distri$Prevalence) * 100
     distri$"Victim_%"[2:nrow(distri)] <-
         prop.table(distri[2:nrow(distri),"Prevalence"]) * 100
     distri$"Incident_%" <- prop.table(distri$Incidence) * 100
-    distri[1,"Incident_%"] <- NA
 
-    # Repeats option HERE
-    if(repeats == TRUE) {distype <- "Repeat victimisation "}
-    else {distype <- "Victimisation "}
+    distri[1,"Incidence"] <- NA
+    distri[1:2,"Repeats"] <- NA
+    distri[1,"Incident_%"] <- NA
 
     if(print_option[1] %in% c("markdown", "pandoc", "latex", "html"))
     {
-        legend <- "distribution of "
-        legend <- paste(distype, legend, sep = "")
+        legend <- "Victimisation distribution of "
         cap <- paste(legend, xname, sep = "")
         cap <- paste(cap, ".", sep = "")
 
@@ -39,5 +38,6 @@ victim_table <- function(x, data = NULL, repeats = FALSE, print_option =
 
     if(!print_option[1] %in% c("none", "markdown", "pandoc", "latex", "html"))
     {stop("Printing method does not exist.")}
+
     return(results)
 }
