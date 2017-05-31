3b1f52152a8bb22e172811d7e49b2b24e7d3c9e4
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-05-31 22:05:41 +0100

    added a new function

---
+++ R/get_glmmadmb.R
@@ -0,0 +1,42 @@
+#' Get parameters from glmmADMB models
+#'
+#' This function returns a list of model parameters from
+#' models created with glmmADMB.
+#' @param mod a model fitted with glmmADMB
+#' @export
+#' @examples
+#' require(glmmadmb)
+#' model <- glmmadmb(extortions ~ 1, data = testdata, family = "poisson")
+#'
+#' get_glmmadmb(model)
+
+
+get_glmmadmb <- function(mod)
+{
+    if("glmmADMB"%in% rownames(installed.packages()) == FALSE)
+        warning("This function only works with glmmadmb objects;
+                it appears you do not have this package installed.")
+
+    if(class(mod) != "glmmadmb")
+        stop(paste0(deparse(substitute(mod)),
+                    " is not a glmmadmb object."))
+
+    alpha <- 1/mod$alpha
+
+    aic <- AIC(mod)
+    bic <- BIC(mod)
+    llik <- logLik(mod)
+
+    results <- list(model = mod$formula, logLik =  as.numeric(llik),
+                    df = attr(llik, "df") ,AIC = aic, BIC = bic, alpha = alpha)
+
+    if(!is.null(mod$random))
+    {
+        var_j <- as.numeric(mod$S)^2
+        results$var_j <- var_j
+
+        ICC <- var_j/(var_j + alpha)
+        results$ICC <- ICC
+    }
+    return(results)
+}
