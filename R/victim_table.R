10d3e03c0be31b6d171a5727859460b5773e3c20
Patricio Rodrigo Estévez Soto <patricio.estevez.14@ucl.ac.uk>
2020-01-27 16:12:35 +0000

    added a Repeats % column

--- R/victim_table.R
+++ R/victim_table.R
@@ -34,6 +34,7 @@ victim_table <- function(x, data = NULL, print_option = c("none",
     distri$"Victim_%"[2:nrow(distri)] <-
         prop.table(distri[2:nrow(distri),"Prevalence"]) * 100
     distri$"Incident_%" <- prop.table(distri$Incidence) * 100
+    distri$"Repeats_%" <- prop.table(distri$Repeats) * 100
 
     distri[1,"Incidence"] <- NA
     distri[1:2,"Repeats"] <- NA
